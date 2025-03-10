577e77cba5bcfb0716ec5e0ff6186c934eea6b15
Ignacio Eleazar Marceca <imarceca@gmail.com>
2025-03-11 00:46:18 +0100

    Header & Signup features completed
    #CTFLTEST-1 (Sign up)
    # CTFLTEST-6 (Header)
    features and steps now test for happy path and invalid imputs

--- cypress/e2e/features/signup.feature
+++ cypress/e2e/features/signup.feature
@@ -1,21 +1,36 @@
 #CTFLTEST-1 (Sign up)
-# As a student, I want to signup in the ISTQB CTFL site with my name and surname, So that  I can access the test.
-
-# Acceptance Criteria 
-# Form should have “Signup“ as Title.
-# Form must validate  both mandatory fields “Name” and “Surname” having:
-# Between 3 and 30 characters.
-# Can not be empty.
-# On send, the form must show a welcome message including the user’s name “Welcome [NAME] [SURNAME] !”.
 # Form must be hidden after signup is complete.
 # ISTQB Tests must be shown after signup is complete.
 
 Feature: Signup new user
 As a student, I want to signup in the ISTQB CTFL site with my name and surname, so that  I can access the test.
 
-Scenario: See Sign up information
-    Given the user visits ISTQB CTFL homepage
-    Then the text "Sign up" should be visible
-    And form must have "Name" on first field
-    And form must have "Surname" on second field
+Background:
+     Given the user visits ISTQB CTFL homepage
 
+Scenario: See sign up information
+    Then the text "Sign up" should be visible
+    And form must have "Name" as title in name form field
+    And form must have "Surname" as title on surname form field
+
+Scenario: Fill sign up form with mandatory fields happy path
+    When the user fill name with "Ben"
+    And the user fill surname with "Cox" 
+    And the user clicks on signup submit
+    Then the user should see welcome message "Welcome"
+
+Scenario Outline: Fill sign up form with invalid data
+    When the user fill names with <name>
+    And the user fill surnames with <surname> 
+    And the user clicks on signup submit_
+    Then the user should see error message "Name and Surname must be between 3 and 30 characters long"
+
+Examples:
+    | name      | surname       | 
+    | "Ed"      | "Richards"    |
+    | "Michael" | "Li"          |
+    | "Al"      | "Wu"          |
+    |    " "    | "Richards"    |
+    | "Michael" |        " "    |
+    | "Sriwattanapongseboripattarakamphuatsigiannopoulos" | "Richards"   |
+    | "Michael" | "Wolfeschlegelsteinhausenbergerdorffsigiannopoulos"    |
--- cypress/e2e/step_definitions/signup_steps.js
+++ cypress/e2e/step_definitions/signup_steps.js
@@ -4,17 +4,68 @@ import {
     Then,
   } from "@badeball/cypress-cucumber-preprocessor";
   
+  // See sign up information
   Then("the text {string} should be visible", (signup_title) => {
-    cy.get('[data-testid="signup_title"]').should('have.text', signup_title);
-
+    cy.get('[data-testid="signup_title"]')
+      .should('have.text', signup_title);
   });
 
-  Then("form must have {string} on first field", (signup_name) => {
-    cy.get('[data-testid="signup_name"]').should('have.attr', 'placeholder', signup_name);
+  Then("form must have {string} as title in name form field", (signup_placeholder_name) => {
+    cy.get('[data-testid="signup_name"]')
+      .should('have.attr', 'placeholder', signup_placeholder_name);
   });
 
-  Then("form must have {string} on second field", (signup_surname) => {
-    cy.get('[data-testid="signup_surname"]').should('have.attr', 'placeholder', signup_surname);
+  Then("form must have {string} as title on surname form field", (signup_placeholder_surname) => {
+    cy.get('[data-testid="signup_surname"]')
+      .should('have.attr', 'placeholder', signup_placeholder_surname);
   });
   
+  //Fill sign up form with mandatory fields happy path
+  When("the user fill name with {string}", (name) => {
+    cy.get('[data-testid="signup_name"]')
+      .clear();
+    cy.get('[data-testid="signup_name"]')
+      .type(name);
+  });
   
+  When("the user fill surname with {string}", (surname) => {
+    cy.get('[data-testid="signup_surname"]')
+      .clear();
+    cy.get('[data-testid="signup_surname"]')
+      .type(surname);
+  });
+
+  When("the user clicks on signup submit", () => {
+    cy.get('[data-testid="singup_submit"]')
+      .click();
+  });
+
+  Then("the user should see welcome message {string}", (welcome_message) => {
+    cy.get('[data-testid="welcome_message"]').children()
+      .should('contain', welcome_message);
+  });
+
+  //Fill sign up form with invalid data
+  When("the user fill names with {string}", (name) => {
+    cy.get('[data-testid="signup_name"]')
+      .clear();
+    cy.get('[data-testid="signup_name"]')
+      .type(name);
+  });
+  
+  When("the user fill surnames with {string}", (surname) => {
+    cy.get('[data-testid="signup_surname"]')
+      .clear();
+    cy.get('[data-testid="signup_surname"]')
+      .type(surname);
+  });
+
+  When("the user clicks on signup submit_", () => {
+    cy.get('[data-testid="singup_submit"]')
+      .click();
+  });
+
+    Then("the user should see error message {string}", (error_message) => {
+    cy.get('[data-testid="singup_error_message"]')
+      .should('contain', error_message);
+  });
